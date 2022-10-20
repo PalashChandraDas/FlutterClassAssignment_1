3ce17c934a362da93bb693fb92070836db0d7fc2
Palash Chandra Das <palashdimik@gmail.com>
2022-10-21 00:49:02 +0600

    lecture_1

---
+++ bin/dart_programming_class.dart
@@ -0,0 +1,20 @@
+void main(){
+
+  //lecture_01
+  int num1, num2, sum, sub, multiply;
+  num1 = 100;
+  num2 = 20;
+
+  //calculating here
+  sum = num1 + num2;
+  sub = num1 - num2;
+  multiply = num1 * num2;
+  double division = num1 / num2;
+
+  //result here
+  print("Sum of $num1 and $num2 = $sum");
+  print("Subtraction of $num1 and $num2 = $sub");
+  print("Multiplication of $num1 and $num2 = $multiply");
+  print("Division of $num1 and $num2 = $division");
+
+}
